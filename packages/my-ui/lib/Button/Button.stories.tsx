b8163e903604f13257cb9a88e90a70793b4ed2ac
GYUJEONG HWANG <octave08b@gmail.com>
2021-11-01 13:33:13 +0900

    fest(my-ui): implement storybook

---
+++ packages/my-ui/lib/Button/Button.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Button from "./Button";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+} as Meta;
+
+export const Text: React.VFC = () => (
+  <Button onClick={action("clicked")}>Hello Button</Button>
+);
+
+export const Emoji: React.VFC = () => (
+  <Button onClick={action("clicked")}>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+  </Button>
+);
