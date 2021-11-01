3b7e7428bc16db3004842555f93a4508ae5ea5cb
GYUJEONG HWANG <octave08b@gmail.com>
2021-11-01 11:53:44 +0900

    feat: add eslint-config-react package

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Fix eslint shareable config (https://github.com/eslint/eslint/issues/3458)
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "cypress", "jest"],
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      jest: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:cypress/recommended',
+      "plugin:jest/recommended"
+    ],
+    settings: {
+      jest: {
+        version:27
+      }
+    }
+}
