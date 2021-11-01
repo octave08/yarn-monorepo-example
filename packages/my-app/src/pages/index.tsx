61b9cac8fa1c3657215d4403daed7f6b170dac32
GYUJEONG HWANG <octave08b@gmail.com>
2021-11-01 14:22:47 +0900

    feat(my-app): add my-app package

---
+++ packages/my-app/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+import styles from "./home.module.css";
+
+import { Button } from "@octave08/my-ui";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>lerna-nextjs-storybook</title>
+      </Head>
+      <div>lerna-nextjs-storybook</div>
+      <Button data-cy="button" onClick={() => alert("clicked")}>
+        Hello World
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
